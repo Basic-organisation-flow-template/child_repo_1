94a00dcc76c4930030412102a936278731bd5bdb
disha-d-github <disha.d.github@gmail.com>
2025-09-03 06:42:25 +0530

    test

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from my_app_project.app import add, subtract, multiply
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(10, 20) == -10
+    assert subtract(0, 0) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 5) == -5
+    assert multiply(0, 100) == 0
